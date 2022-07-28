3475a2031ffd97e29a6f869a36bf19b375414142
André Filippe dos Santos <andre.filippe@after.sale>
2022-07-28 16:07:04 -0300

    feat: created script send farm with b

---
+++ click-send-farm-b.js
@@ -0,0 +1,20 @@
+javascript:
+if (!window.location.href.includes("screen=am_farm")) {
+    window.location.href = game_data.link_base_pure + "am_farm"
+}
+
+if (typeof macroSendFarm !== "undefined") clearInterval(macroSendFarm);
+
+index = 0;
+buttonsSendB = document.querySelectorAll('#plunder_list .farm_icon_b');
+function sendFarm() {
+    let cLcurrentAmount = document.getElementById('light').innerHTML
+    if (cLcurrentAmount >= 2 && buttonsSendB[index]) {
+        buttonsSendB[index].click();
+        index++;
+    } else {
+        clearInterval(macroSendFarm)
+    }
+}
+
+macroSendFarm = setInterval(sendFarm, 250);
